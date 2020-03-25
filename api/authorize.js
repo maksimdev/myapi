6e010e42d3d99c49d42ebda45f597f898516235b
Maksim Ashmetev <maksim.ashmetev@novartis.com>
2020-03-25 23:20:00 +0300

    Auth works well

--- api/authorize.js
+++ api/authorize.js
@@ -2,39 +2,17 @@ const jwt = require('jsonwebtoken');
 const utils = require('../lib/utils');
 
 module.exports.authorize = function(event, context, callback) {
-  var token = 'allow'; //event.authorizationToken;
-
-  switch (token) {
-      case 'allow':
-          callback(null, utils.generatePolicy('user', 'Allow', event.methodArn));
-          break;
-      case 'deny':
-          callback(null, utils.generatePolicy('user', 'Deny', event.methodArn));
-          break;
-      case 'unauthorized':
-          callback("Unauthorized");   // Return a 401 Unauthorized response
-          break;
-      default:
-          callback("Error: Invalid token"); // Return a 500 Invalid token response
+  try {
+    const token = event.authorizationToken.slice(7);
+    const { user } = jwt.verify(token, process.env.JWT_SECRET);
+    const authorizerContext = { user: JSON.stringify(user) };
+    const isAllowed = utils.authorizeUser(user.scopes, event.methodArn);
+    if (isAllowed) {
+      callback(null, utils.generatePolicy('user', 'Allow', event.methodArn, authorizerContext, authorizerContext));
+    } else {
+      callback(null, utils.generatePolicy('user', 'Deny', event.methodArn, authorizerContext, authorizerContext)); //404 we can change policies just rename to get 404 lambda!
+    }
+  } catch (e) {
+    callback(`Unauthorized`); //404 we can change policies just rename to get 404 lambda!
   }
 };
-
-//==========================================================
-// const jwt = require('jsonwebtoken');
-// const utils = require('../lib/utils');
-
-// module.exports.authorize = (event, context, callback) => {
-//   const token = event.authorizationToken;
-
-//   try {
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//     const user = decoded.user;
-
-//     const authorizerContext = { user: JSON.stringify(user) };
-//     const policyDocument = utils.buildIAMPolicy(user.id, 'Allow', event.methodArn, authorizerContext);
-
-//     callback(null, policyDocument);
-//   } catch (e) {
-//     callback('Unauthorized');
-//   }
-// };
--- api/login.js
+++ api/login.js
@@ -1,24 +1,10 @@
-// /functions/login.js
-//вынести логику по получению пользователя и обернуть в трай кетч!
 const config = require('./../config.json');
 const { Pool } = require('pg');
 const jwt = require('jsonwebtoken');
-//const users = require('../lib/users');
 const utils = require('../lib/utils');
 
 const pool = new Pool(config);
-const JWT_EXPIRATION_TIME = '5m';
 
-/**
-  * POST /sessions
-  *
-  * Returns a JWT, given a username and password.
-  * @method login
-  * @param {String} event.body.username
-  * @param {String} event.body.password
-  * @throws Returns 401 if the user is not found or password is invalid.
-  * @returns {Object} jwt that expires in 5 mins
-  */
 module.exports.login = async (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
 
@@ -37,15 +23,13 @@ module.exports.login = async (event, context, callback) => {
 
   if(!user) return callback(null, utils.convertToRespose('Error: username or/and password is not valid', 404));
 
-  //callback(null, utils.convertToRespose(result.rows[0]));
-
   try {
-    // Authenticate user
-    //const user = users.login(username, password); //checking user should be there
 
-    // Issue JWT
-    const token = jwt.sign({ user }, process.env.JWT_SECRET, { expiresIn: JWT_EXPIRATION_TIME });
-    const response = { // Success response
+    const token = jwt.sign(
+      { user, iat: Date.now(), exp: Date.now() + 86400000 },
+      process.env.JWT_SECRET
+    );
+    const response = {
       statusCode: 200,
       headers: {
         'Access-Control-Allow-Origin': '*',
